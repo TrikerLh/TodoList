82de611323e376bf2df53d8021ea82d10358eb3b
Raúl Corchero <rcorchero@aidacanarias.com>
2024-10-11 23:20:31 +0100

    write aceptance test

--- ToDoList/ToDoList.Api.Tests/ToDoListAcceptanceTest.cs
+++ ToDoList/ToDoList.Api.Tests/ToDoListAcceptanceTest.cs
@@ -1,4 +1,5 @@
 using System.Net;
+using System.Text;
 using FluentAssertions;
 using NUnit.Framework.Internal;
 
@@ -35,6 +36,33 @@ namespace ToDoList.Api.Tests {
 			var response = await client.GetAsync("WeatherForecast");
 			var content = await response.Content.ReadAsStringAsync();
 			response.StatusCode.Should().Be(HttpStatusCode.OK);
+		}
+
+		[Test]
+		public async Task shouldAllowAddingTaskCompleteAndRetrieveTheList()
+		{
+			var taskDescription = "Write a test that fails";
+			await client.PostAsync("todo", new StringContent(taskDescription, Encoding.UTF8, "application/json"));
+			taskDescription = "Write Production code that makes the test pass";
+			await client.PostAsync("todo", new StringContent(taskDescription, Encoding.UTF8, "application/json"));
+			taskDescription = "Refactor if there is opportunity";
+			await client.PostAsync("todo", new StringContent(taskDescription, Encoding.UTF8, "application/json"));
+			var requestUri = "todo/1";
+			await client.PatchAsync(requestUri, new StringContent("done", Encoding.UTF8, "application/json"));
+
+			var response = await client.GetAsync("todo");
+
+			var content = await response.Content.ReadAsStringAsync();
+			var expectedList = new List<string>
+			{
+				"[X] 1. Write a test that fails",
+				"[ ] 2. Write Production code that makes the test pass",
+				"[ ] 3. Refactor if there is opportunity"
+			};
+
+			response.StatusCode.Should().Be(HttpStatusCode.OK);
+			content.Should().BeEquivalentTo(expectedList.ToString());
+
 
 		}
 	}
